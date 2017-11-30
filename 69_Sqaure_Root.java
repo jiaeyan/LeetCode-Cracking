d155a53bbc62dcf0ac2a7546825fc57523dffa41
Jiajie Yan <svenyan234@gmail.com>
2017-11-30 17:49:13 -0500

    Update 69_Sqaure_Root.java

--- 69_Sqaure_Root.java
+++ 69_Sqaure_Root.java
@@ -36,6 +36,27 @@ public class Sqaure_Root {
     	    return (int)res;
     }
     
+    /*
+     * The sequence 1, 2, ... , n has no duplication.
+     * Near the very end, closest step, before while loop, left = mid = right.
+     * In while, If mid < sqrt(x), left = mid + 1 executed, right pointer is not moving, and right is the answer.
+     * If while, If mid > sqrt(x), right = mid - 1 executed, right pointer shifts left 1, closest to sqrt(x), right is also the answer.
+     */
+//    public int mySqrt(int x) {
+//        int left = 1, right = x;
+//        while (left <= right) {
+//            int mid = left + (right - left) / 2;
+//            if (mid == x / mid) {
+//                return mid;
+//            } else if (mid < x / mid) {
+//                left = mid + 1;
+//            } else {
+//                right = mid - 1;
+//            }
+//        }
+//        return right;
+//    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
