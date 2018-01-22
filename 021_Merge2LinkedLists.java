dd687329183d61a02c79dba032794c4e08cd60f2
Jiajie Yan <svenyan234@gmail.com>
2018-01-21 22:58:37 -0500

    Update 021_Merge2LinkedLists.java

--- 021_Merge2LinkedLists.java
+++ 021_Merge2LinkedLists.java
@@ -11,8 +11,12 @@ import definition.ListNode;
  * Output: 1->1->2->3->4->4
  */
 
+/*
+ * Note each node is sorted, which means we may consume the same 
+ * node all the time to the end. E.g.: l1 = 1->45->67; l2 = 2->3->4->5->6
+ */
+
 public class Merge2LinkedLists {
-	
 	
 	/*
 	 * Set a prehead, and make this the final result. Since we should return
@@ -41,6 +45,34 @@ public class Merge2LinkedLists {
 		curr.next = l1 == null? l2 : l1;
         return prehead.next;
     }
+	
+	
+	/*
+	 * Compare the 1st nodes from l1 and l2, the bigger one becomes
+	 * the head of the result, then cut l1, compare l1.next with l2...
+	 * keep doing the same procedure, until either becomes null, thus
+	 * the return the non-null one directly, which is at the middle or
+	 * very end.
+	 */
+	public ListNode recursive(ListNode l1, ListNode l2) {
+		// l1 is exhausted, l2 is not the given l2, it is some point of 
+		// l2.next.next...
+		if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+		// choose the bigger nodes form l1 and l2, cut the chosen one
+        else if (l1.val < l2.val) {
+            l1.next = recursive(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = recursive(l1, l2.next);
+            return l2;
+        }
+	}
 	
 	public static void main(String[] args) {
 		Merge2LinkedLists ml = new Merge2LinkedLists();
