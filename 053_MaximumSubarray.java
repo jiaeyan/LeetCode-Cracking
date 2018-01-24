08244a46bda849090c3e34d122ec88c63173d067
Jiajie Yan <svenyan234@gmail.com>
2018-01-24 00:52:02 -0500

    Update 053_MaximumSubarray.java

--- 053_MaximumSubarray.java
+++ 053_MaximumSubarray.java
@@ -16,7 +16,32 @@ public class MaximumSubarray {
 	/*
 	 * Dynamic programming.
 	 * 
-	 * Maintain two values, one is local max, 
+	 * At each point in the array, there is a max subarray before it (which
+	 * may not start from 0). So we simply start from 1st item, then extend the
+	 * array one by one, and find the max subarray of that extended subarray, this
+	 * extended subarray must contain the current item.
+	 * 
+	 * Local: the max sum of subarray that ends at current position, the end position must be included; 
+	 *        so when extend to next position, compare (local + next item) vs next item to see which
+	 *        is bigger; if here local > 0, the new local value of extended subarray must be (local + next item); 
+	 *        otherwise, the new local value for next position will be the next item.
+	 *        this ensures the subarray is continuous
+	 * Global: the max of all max sums among different subarrays that end at each position in the array
+	 * 
+	 * [−2,1,−3,4,−1,2,1,−5,4]
+	 * Start from -2, the max subarray to -2 is [-2], local(max sum) = -2;
+	 * extend to 1, the subarray is [-2, 1], since now we are considering the max subarray that
+	 * must contain 1, we compare local + 1 < 1, so the max subarray is [1], local = 1;
+	 * extend to -3, the subarray is [-2, 1, -3], we consider the max subarray that must contain
+	 * -3, so compare local + -3 = -2 > -3, so the max subarray contains -3 is [1, -3], local = -2;
+	 * extend to 4, the subarray is [-2, 1, -3, 4], we consider the max subarray that must contain
+	 * 4, so compare local + 4 < 4, so the max subarray contains 4 is [4], local = 4;
+	 * ...
+	 * So we can see, if the former local < 0, the next max subarray will directly start with the new
+	 * item, because (local + new item) < new item.
+	 * the next max subarrays at each position are [4, -1], [4, -1, 2], [4, -1, 2, 1], [4, -1, 2, 1, -5],
+	 * [4, -1, 2, 1, -5, 4], with local values of 3, 5, 6, 1, 5, so among all these local values,
+	 * the max is 6.
 	 */
 	public int Kadane(int[] nums) {
 		if (nums.length == 0) return 0;
