fa8678260d52ce87a25461f6e702eb2a52c71824
Jiajie Yan <svenyan234@gmail.com>
2018-01-24 21:40:17 -0500

    Update 053_MaximumSubarray.java

--- 053_MaximumSubarray.java
+++ 053_MaximumSubarray.java
@@ -31,7 +31,7 @@ public class MaximumSubarray {
 	 * [−2,1,−3,4,−1,2,1,−5,4]
 	 * Start from -2, the max subarray to -2 is [-2], local(max sum) = -2;
 	 * extend to 1, the subarray is [-2, 1], since now we are considering the max subarray that
-	 * must contain 1, we compare local + 1 < 1, so the max subarray is [1], local = 1;
+	 * must contain 1, we compare local (max subarray ends at previous position) + 1 < 1, so the max subarray is [1], local = 1;
 	 * extend to -3, the subarray is [-2, 1, -3], we consider the max subarray that must contain
 	 * -3, so compare local + -3 = -2 > -3, so the max subarray contains -3 is [1, -3], local = -2;
 	 * extend to 4, the subarray is [-2, 1, -3, 4], we consider the max subarray that must contain
@@ -54,15 +54,81 @@ public class MaximumSubarray {
 		
 		for(int i = 1; i < nums.length; i++) {
 			local = Math.max(nums[i], local + nums[i]);
+//			System.out.println(local);	
 			global = Math.max(global, local);
 		}
 		
 		return global;
     }
 	
+	/*
+     * Divide-and-conquer method.
+     * The maximum summation of subarray can only exists under following conditions:
+     * 1. the maximum summation of subarray exists in left half.
+     * 2. the maximum summation of subarray exists in right half.
+     * 3. the maximum summation of subarray exists crossing the midpoints to left and right. 
+     * 1 and 2 can be reached by using recursive calls to left half and right half of the subarraies. 
+     * Condition 3 can be found starting from the middle point to the left,
+     * then starting from the middle point to the right. Then adds up these two parts and return. 
+     * 
+     * T(n) = 2*T(n/2) + O(n)
+     * this program runs in O(nlogn) time
+    */
+	public int dividenconquer(int[] nums) {
+	    int maxsum = subArray(nums, 0, nums.length-1);
+	    return maxsum;
+	}
+
+	private int subArray(int[] A, int left, int right){
+	    if (left == right){
+	        //base case
+	        return A[left];
+	    }
+	    int mid = left + (right-left)/2;
+	    int leftsum = subArray(A, left, mid); //left part of the subarray sum, condition 1
+	    int rightsum = subArray(A, mid+1, right); //right part of the subarray sum, condition 2
+	    int middlesum = midSubArray(A, left, mid, right); //cross part of the subarray sum, condition 3
+	    // System.out.println(leftsum);
+	    // System.out.println(rightsum);
+	    // System.out.println(middlesum);
+	    
+	    //following if condition will return middlesum if lost the "=" under the conditon of leftsum = rightsum, which may be problematic if leftsum = rightsum < middlesum.
+	    //for example: [-1, -1, -2, -2], a.k.a., not able to handel all negative arrays
+	    if (leftsum >= rightsum && leftsum >= middlesum){
+	        return leftsum;
+	    }
+	    if (rightsum >= leftsum && rightsum >= middlesum){
+	        return rightsum;
+	    }
+	    return middlesum;
+	}
+
+	private int midSubArray(int[] A, int left, int mid, int right){
+	    int leftsum = Integer.MIN_VALUE;
+	    int rightsum = Integer.MIN_VALUE;
+	    int sum = 0;
+	    for (int i = mid; i >= left; i--){
+	        sum += A[i];
+	        if (leftsum < sum){
+	            leftsum = sum;
+	        }
+	    }
+	    
+	    sum = 0;
+	    for (int j = mid + 1; j <= right; j++){
+	        sum += A[j];
+	        if (rightsum < sum){
+	            rightsum = sum;
+	        }
+	    }
+	    
+	    return leftsum + rightsum;
+	}
+	
 	public static void main(String[] args) {
 		MaximumSubarray ms = new MaximumSubarray();
 		System.out.println(ms.Kadane(new int[] {-2,1,-3,4,-1,2,1,-5,4}));
+		System.out.println(ms.dividenconquer(new int[] {-2,1,-3,4,-1,2,1,-5,4}));
 	}
 
 }
