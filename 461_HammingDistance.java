2e0b912e832c42db9a7fe878ebe7d582aff8266a
Jiajie Yan <svenyan234@gmail.com>
2018-02-24 00:10:04 -0500

    Update 461_HammingDistance.java

--- 461_HammingDistance.java
+++ 461_HammingDistance.java
@@ -24,6 +24,32 @@ public class HammingDistance {
 		return Integer.bitCount(x ^ y);
     }
 	
+	// https://tech.liuchao.me/2016/11/count-bits-of-integer/
+	// see explanation of bitcount
+	public int bitCount1(int n) {
+	    int count = 0;
+	    while (n != 0) {
+	        count += n & 1;
+	        n >>= 1;
+	    }
+	    return count;
+	}
+	
+	public int bitCount2(int n) {
+	    int count = 0;
+	    while (n != 0) {
+	        // this operation removes one last significant 1
+	    	    // if the last digit is 0, it will jump to next 1 when do n-1
+	        // n     = 1 1 0 1 0 0 0
+	    	    // n - 1 = 1 1 0 0 1 1 1 (minus 1 from last 1 of n), do n &= n - 1
+	    	    // n     = 1 1 0 0 0 0 0
+	    	    // n - 1 = 1 0 1 1 1 1 1 (skip the last 0, directly to 2nd 1)
+	    	    n &= n - 1;
+	        count++;
+	    }
+	    return count;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
